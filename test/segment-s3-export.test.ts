31fb85eaa1e36670a306a324b6a828f1d7a9426d
Jose Miguel Navarro <jmn8718@gmail.com>
2021-12-22 22:58:31 +0900

    Initial commit

---
+++ test/segment-s3-export.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as SegmentS3Export from '../lib/segment-s3-export-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/segment-s3-export-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new SegmentS3Export.SegmentS3ExportStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
